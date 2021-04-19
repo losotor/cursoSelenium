5fdc95a926d2b6a4f4b76487c606e8252177d66c
Luis Soto Rodriguez <luis.soto_ex@scotiabank.cl>
2021-04-19 13:16:45 -0400

    Se sube ejercicio 3 del practico 8

---
+++ EjercicioP3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EjercicioP3 {
+
+    public static void main (String args []) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int num = input.nextInt();
+
+        int duplicar = calcularDoble(num);
+        System.out.println("El doble del numero ingresado es: " + duplicar);
+
+    }
+
+    public static int calcularDoble (int num) {
+        return num * 2;
+    }
+
+}
